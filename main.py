f3ce3943fed89ff8da32531502b036c9cc972d20
fayazalijamali73-king <fayazalijamali73@gmail.com>
2026-09-20 12:26:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,8 @@ logger = logging.getLogger(__name__)
 # write. .env file or Replit Secrets in BOT_TOKEN set.
 # Example: export BOT_TOKEN="your_token_yahan"
 # ─────────────────────────────────────────────────────────────────────────────
-TOKEN = os.getenv("BOT_TOKEN", "8959543875:AAEY-XIwlxl4CbaP-7dN_rxY97FLfRnoegU")  # Set BOT_TOKEN in the environment; never hardcode the token
+TOKEN = os.getenv("BOT_TOKEN", "
+8959543875:AAFi7zMaZeIYEyGtr_GLpDNkjdbmKQnTSlE")  # Set BOT_TOKEN in the environment; never hardcode the token
 if not TOKEN:
     raise RuntimeError(
         "❌ BOT_TOKEN environment variable is not set!\n"
